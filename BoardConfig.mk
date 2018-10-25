f1b2c591413923338d53910f15e8f9b6331d9488
Jviix <jviix8gmail.com>
2018-10-25 15:05:29 +0000

    Bringup to LOS 15.1
    Change-Id: I1e44d6edbd046f5ae6c9cd3f9973f2d92e5113f5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (c) 2018, The LineageOS Project.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,27 +18,26 @@
 
 TARGET_OTA_ASSERT_DEVICE := trltexx,trltedt
 
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 262144
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 17825792 
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 19922944 
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3774873600 #910T uses: 3984588800
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 27019685888 #910T uses: 26558312448
+# Camera
+TARGET_FIXUP_PREVIEW := true
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := apq8084_sec_trlte_eur_defconfig
 
-# Camera
-TARGET_FIXUP_PREVIEW := true
-
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_INIT_VENDOR_LIB := libinit_apq8084
 TARGET_LIBINIT_DEFINES_FILE := device/samsung/trltexx/init/init_trlte.cpp
-TARGET_UNIFIED_DEVICE := true
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 262144
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 17825792
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 19922944
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3984588800
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 26558312448
 
 # Radio
-BOARD_RIL_CLASS := ../../../device/samsung/trltexx/ril
+BOARD_RIL_CLASS := $(LOCAL_DIR)/ril
 
 # inherit from the proprietary version
 -include vendor/samsung/trltexx/BoardConfigVendor.mk
